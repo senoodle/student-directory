b7b03624e373cd6fb39349cafa0f4f8a3f329572
Sibel Eren <edaeren575@gmail.com>
2021-06-14 14:01:05 +0100

    added list of students

---
+++ directory.rb
@@ -0,0 +1,19 @@
+# printing the list of students names
+puts "The students of Villains Academy"
+puts "--------------"
+puts "Dr Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+#printing the number of students
+print "Overall, we have "
+print 11 #important that print doesnt add new line characters
+puts " great students"
